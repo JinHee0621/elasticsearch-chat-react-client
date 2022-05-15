e63687e29f5e9faa751a0191a88523c4673b68ef
unknown <lsd0017@naver.com>
2022-05-15 14:05:54 +0900

    add Chatting

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from "react";
-import image from "../image/sample_1.jpg";
+import image from "../image/titleImage.png";
 import "../css/Login.css";
 import io from "socket.io-client";
 const socket = io("http://localhost:3001/");  //3001 Back단 서버포트
--- src/routes/Room.js
+++ src/routes/Room.js
@@ -1,26 +1,38 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { useState, useEffect } from "react";
 import io from "socket.io-client";
 const socket = io("http://localhost:3001/");  //3001 Back단 서버포트
 
 function Room({ location, history }) {
     const [roomid, setRoomId] = useState("");
+    const [message, setMessage] = useState("");
+    const [messageLog, setLog] = useState([]);
 
     useEffect(() => {
         setRoomId(location.room_id);
-        socket.emit('enterRoom', location.room_id); 
+        socket.emit('enterRoom', location.room_id);
     }, [])
 
     useEffect(() => {
         socket.on('recept_message', (message) => {
-            console.log("메세지 수신 : " + message )
+            addMessage('otherUser', message, 'other')
         });
     }, [])
 
-    const [message, setMessage] = useState("");
+    const addMessage = (name , message, target) => {
+        let message_div = {
+            "userName" : name,
+            "message" : message,
+            "user" : target
+        }
+        setLog((currentArray) => [ ...currentArray,message_div])
+    }
+
     const onInputMessage = (event) => setMessage(event.target.value);
     const doSend = (event) => {
         socket.emit('send', message, roomid); // check 요청
+        addMessage(location.room_id, message, 'me')
         setMessage("");
         event.preventDefault();
     }
@@ -31,6 +43,12 @@ function Room({ location, history }) {
         </div>
         <div>
             <form onSubmit={doSend}>
+                <div id="message_window">
+                    {messageLog.map((message_div, index) => (
+                        <div className={message_div.user} key={index}>{message_div.userName} : {message_div.message}</div>
+                    ))
+                    }
+                </div>
                 <div>
                     <input onChange={onInputMessage} type="text" value={message} id="user_message" placeholder="메시지를 입력하세요" /><br />
                 </div>
